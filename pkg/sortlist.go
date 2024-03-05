31ab012563fa5360b43da92143d9b02071d38788
halabboo <husain.akarim@gmail.com>
2024-03-05 19:48:50 +0300

    edit sort func

--- pkg/sortlist.go
+++ pkg/sortlist.go
@@ -8,10 +8,10 @@ func SortList(list []FileInfo) {
 	for len(list)-1 > i {
 		name0 := FilterFileName(list[i].Name)
 		name1 := FilterFileName(list[i+1].Name)
-		if len(name0) > 1 {
+		if name0 != "." && name0 != ".." {
 			name0 = strings.TrimPrefix(name0, ".")
 		}
-		if len(name1) > 1 {
+		if name1 != "." && name1 != ".." {
 			name1 = strings.TrimPrefix(name1, ".")
 		}
 		if strings.ToLower(name0) > strings.ToLower(name1) {
